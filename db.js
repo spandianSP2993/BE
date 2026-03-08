3cf0f8e53e9a9ac2219b523fa7ed44590e045365
spandianSP2993 <spandian2993@outlook.com>
2026-03-08 21:46:11 +0530

    feat: configure database connection using environment variables.

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
+require('dotenv').config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "File_Management_System_Test",
-  password: "PostgresServer",
-  port: "5432",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.connect((err, client, release) => {
